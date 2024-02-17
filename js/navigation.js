ab6bc06a312dd5165f16adc568d76f2a9be65aa3
Responsibleuk <james@story-x.co.uk>
2024-02-17 16:55:10 +0000

    Add

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,20 @@
-// navigation.js
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Function to fetch and inject HTML content
+  const fetchAndInjectHTML = (url, targetId) => {
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+        // Inject the fetched HTML into the designated target
+        document.getElementById(targetId).innerHTML = html;
+      })
+      .catch(error => {
+        console.error(`Error fetching ${url}:`, error);
+      });
+  };
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the placeholder element
-    var navigationPlaceholder = document.getElementById('navigation-placeholder');
-  
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-  
-    // Configure it to fetch the navigation.html file
-    xhr.open('GET', 'navigation.html', true);
-  
-    // Set the callback function to handle the response
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Insert the navigation HTML into the placeholder
-        navigationPlaceholder.innerHTML = xhr.responseText;
-      }
-    };
-  
-    // Send the request
-    xhr.send();
-  });
+  // Load navigation
+  fetchAndInjectHTML('nav.html', 'navigation');
+
+  // Load footer
+  fetchAndInjectHTML('footer.html', 'footer');
+});
